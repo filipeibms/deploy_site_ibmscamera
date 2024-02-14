e2cd695111b870a57a92224088ecdb58299ff30f
filipeibms <33269414+filipeibms@users.noreply.github.com>
2024-02-14 17:03:03 -0300

    arquivo add

--- meu_site_ibms.py
+++ meu_site_ibms.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import cv2
 
 app = Flask(__name__)
-# route -> hashtagtreinamentos.com/
-# função -> o que você quer exibir naquela página
-# template
 
+# Função para gerar os quadros do vídeo
+def gen_frames():
+    video_capture = cv2.VideoCapture(0)  # Inicializa a captura de vídeo
+
+    while True:
+        success, frame = video_capture.read()  # Lê um quadro do vídeo
+
+        if not success:
+            break
+        else:
+            # Codifica o frame como JPEG
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+# Define a rota para o streaming de vídeo
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# Define a rota para a página inicial
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
@@ -17,6 +37,5 @@ def contatos():
 def usuarios(nome_usuario):
     return render_template("usuarios.html", nome_usuario=nome_usuario)
 
-# colocar o site no ar
 if __name__ == "__main__":
     app.run(debug=True)
